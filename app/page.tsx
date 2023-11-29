a6eb1c2a10b23ef532a0b1ff1b71a4964df17dfe
Dimitar <dimitar.mandarliev@gmail.com>
2023-11-29 15:20:27 +0100

    Creating the User Button Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="">
       <h1>Dropbox clone</h1>
+      <UserButton />
     </main>
   );
 }
