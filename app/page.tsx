dbc5adc1a03ff49f7c0af0ff88f85936beccd1d2
Dimitar <dimitar.mandarliev@gmail.com>
2023-11-30 09:09:35 +0100

    Building the Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <main className="">
       <h1>Dropbox clone</h1>
-      <UserButton />
     </main>
   );
 }
