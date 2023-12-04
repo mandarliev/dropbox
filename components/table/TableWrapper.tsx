48196d134972e6d883530945693e75e4a3b089e7
Dimitar <dimitar.mandarliev@gmail.com>
2023-12-04 18:04:43 +0200

    Implementing the Sort Functionality

--- components/table/TableWrapper.tsx
+++ components/table/TableWrapper.tsx
@@ -5,19 +5,45 @@ import { Button } from "../ui/button";
 import { DataTable } from "./Table";
 import { columns } from "./columns";
 import { useUser } from "@clerk/nextjs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { collection, orderBy, query } from "firebase/firestore";
+import { db } from "@/firebase";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 function TableWrapper({ skeletonFiles }: { skeletonFiles: FileType[] }) {
   const { user } = useUser();
   const [initialFiles, setInitialFiles] = useState<FileType[]>([]);
   const [sort, setSort] = useState<"asc" | "desc">("desc");
 
+  const [docs, loading, error] = useCollection(
+    user &&
+      query(
+        collection(db, "users", user.id, "files"),
+        orderBy("timestamp", sort)
+      )
+  );
+
+  useEffect(() => {
+    if (!docs) return;
+
+    const files = docs.docs.map((doc) => ({
+      id: doc.id,
+      filename: doc.data().filename || doc.id,
+      timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+      fullName: doc.data().fullName,
+      downloadURL: doc.data().downloadURL,
+      type: doc.data().type,
+      size: doc.data().size,
+    }));
+    setInitialFiles(files);
+  }, [docs]);
+
   return (
     <div>
       <Button onClick={() => setSort(sort === "desc" ? "asc" : "desc")}>
         Sort By {sort === "desc" ? "Newest" : "Oldest"}
       </Button>
-      <DataTable columns={columns} data={skeletonFiles} />
+      <DataTable columns={columns} data={initialFiles} />
     </div>
   );
 }
